d5b1068b1aef79d8f039762f17e7f814e5cf4209
Rubén de Celis Hernández <contact@rdch106.hol.es>
2017-09-29 11:28:50 +0200

    Replaced '' with "" (code style consistency)

--- js/common.js
+++ js/common.js
@@ -12,7 +12,7 @@ function printRanking(ranking){
             
     }*/
     ranking.forEach(function(arrayItem){
-            $('#ranking tbody').append("<tr><td><a href='https://github.com/"+arrayItem.name+"' target='_blank'>"+arrayItem.name+"</a>"
+            $("#ranking tbody").append("<tr><td><a href='https://github.com/"+arrayItem.name+"' target='_blank'>"+arrayItem.name+"</a>"
             +"</td><td>"+arrayItem.public+"</td><td>"+arrayItem.private+"</td><td>"
             +arrayItem.repositories+"</td><td>"+arrayItem.followers+"</td><td>"+arrayItem.join+"</td><td>"
             +"<img width='64px' src='"+arrayItem.avatar+"'>"+"</td></tr>");
@@ -22,11 +22,11 @@ function printRanking(ranking){
     
 function get_ghranking(region){
     
-    $.ajax({type:'GET', url: "https://raw.githubusercontent.com/iblancasa/ghrankings/master/"+region+".json", dataType: 'json', async: true,
+    $.ajax({type:"GET", url: "https://raw.githubusercontent.com/iblancasa/ghrankings/master/"+region+".json", dataType: "json", async: true,
         success: function(data) {
             $(".datetimestamp").html(data.date);
             printRanking(data.users);
-            ranking =$('#ranking').DataTable( 
+            ranking =$("#ranking").DataTable( 
             {
             "language": {
                 "url": "i18n/Spanish.json"
@@ -41,19 +41,19 @@ function change(value, name) {
     if(ranking !== undefined){
         ranking.destroy();
     }
-    $('#ranking tbody').empty();
+    $("#ranking tbody").empty();
     get_ghranking(value);
 }
 
 function sortSelect(select, attr, order) {
-    if(attr === 'text'){
-        if(order === 'asc'){
-            $(select).html($(select).children('option').sort(function (x, y) {
+    if(attr === "text"){
+        if(order === "asc"){
+            $(select).html($(select).children("option").sort(function (x, y) {
                 return $(x).text().toUpperCase() < $(y).text().toUpperCase() ? -1 : 1;
             }));
         }// end asc
-        if(order === 'desc'){
-            $(select).html($(select).children('option').sort(function (y, x) {
+        if(order === "desc"){
+            $(select).html($(select).children("option").sort(function (y, x) {
                 return $(x).text().toUpperCase() < $(y).text().toUpperCase() ? -1 : 1;
             }));
         }// end desc
@@ -63,20 +63,20 @@ function sortSelect(select, attr, order) {
 function init(region){
     
     $(document).ready(function() {
-        $('#selection').on('change', function() {
-            change($(this).val(), $(this).find('option:selected').text());
+        $("#selection").on('change', function() {
+            change($(this).val(), $(this).find("option:selected").text());
         });
     });
     
     $(document).ready(function () {
-            sortSelect('#selection', 'text', 'asc');
+            sortSelect("#selection", "text", "asc");
             get_ghranking(region);
             $("#selection").val(region);    
     });
     
     $(document).ready(function() {
         $("#theme").val("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");  // Default theme
-        $('#theme').on('change', function() {            
+        $("#theme").on('change', function() {            
             $("#css_theme").attr("href", $(this).val());
         });
     });
