757dd5055b28a5a52b6e8e9a7265bae21f55b66a
Rubén de Celis Hernández <contact@rdch106.hol.es>
2017-11-19 19:22:14 +0100

    Add region param to initial region visualization

--- js/common.js
+++ js/common.js
@@ -43,6 +43,18 @@ function get_ghranking(region){
     });
 }
 
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    //url = url.toLowerCase(); // This is just to avoid case sensitiveness  
+    //name = name.replace(/[\[\]]/g, "\\$&").toLowerCase();// This is just to avoid case sensitiveness for query parameter name
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 function change(value, name) {    
     if(ranking !== undefined){
         ranking.destroy();
